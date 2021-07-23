562b01d04354709c4bc09698c3b462e3fa8861f4
Ken Karlson <kenkarlson@mail.ru>
2021-07-23 23:08:25 +0300

    add trailer buttons

--- js/main.js
+++ js/main.js
@@ -1,104 +1,163 @@
 const getElement = (tagName, classNames, attributes) => {
-		const element = document.createElement(tagName);
-		if(classNames){
-			element.classList.add(...classNames);
+	const element = document.createElement(tagName);
+	if (classNames) {
+		element.classList.add(...classNames);
+	}
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute];
 		}
-		if(attributes){
-				for(const attribute in attributes){
-					element[attribute] = attributes[attribute];
-				}
-		}
-		return element;
+	}
+	return element;
 };
 //Add classes and images to head?
 const createHeader = (param) => {
-		const header = getElement('header');
-		const container = getElement('div', ['container']);
-		const wrapper = getElement('div', ['header']);
-//logo img
-		if(param.header.logo){
-			const logo = getElement('img', ['logo'], {
-				src: param.header.logo,
-				alt: 'Логотип ' + param.title,
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+	//logo img
+	if (param.header.logo) {
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: 'Логотип ' + param.title,
+		});
+		wrapper.append(logo);
+	};
+	//menu link
+	if (param.header.menu) {
+		const nav = getElement('nav', ['menu-list']);
+		const allMenuLink = param.header.menu.map(item => {
+			const link = getElement('a', ['menu-link'], {
+				href: item.link,
+				textContent: item.title
 			});
-			wrapper.append(logo);
-		};
-//menu link
-		if(param.header.menu){
-			const nav = getElement('nav', ['menu-list']);
-			const allMenuLink = param.header.menu.map(item => {
-				const link = getElement('a', ['menu-link'], {
-					href: item.link,
-					textContent: item.title
-				});
-				return link;
-			});
-			nav.append(...allMenuLink);
-			wrapper.append(nav);
-		};
-//social link
-		if(param.header.social){
-			const socialWrapper = getElement('div', ['social']);
-			const allSocial = param.header.social.map(item => {
-					const socialLink = getElement('a', ['social-link']);
-					socialLink.append(getElement('img', [], {
-						src: item.image,
-						alt: item.title,
-					}));
-					socialLink.href = item.link;
-					return socialLink;
-			});
-			
-			socialWrapper.append(...allSocial);
-			wrapper.append(socialWrapper);
-		};
-// add all to page		
+			return link;
+		});
+		nav.append(...allMenuLink);
+		wrapper.append(nav);
+	};
+	//social link
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title,
+			}));
+			socialLink.href = item.link;
+			return socialLink;
+		});
 
-		header.append(container);
-		container.append(wrapper);
-		return header;
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	};
+	// add all to page		
+
+	header.append(container);
+	container.append(wrapper);
+	return header;
 };
 // main
-const createMain = ({ title, main: {genre, rating, description, trailer}}) =>{
+const createMain = ({
+	title,
+	main: {
+		genre,
+		rating,
+		description,
+		trailer
+	}
+}) => {
 	const main = getElement('main');
-	const container = getElement('div', ['container'] );
+	const container = getElement('div', ['container']);
 	main.append(container);
-	const wrapper = getElement('div', ['main-content'] );
+	const wrapper = getElement('div', ['main-content']);
 	container.append(wrapper);
-	const content = getElement('div', ['content'] );
+	const content = getElement('div', ['content']);
 	wrapper.append(content);
-	if(genre){
-			const genreSpan = getElement('span', 
-			['genre','animated', 'fadeInRight'],
-			{textContent: genre}
-			);
-			content.append(genreSpan);
+	if (genre) {
+		const genreSpan = getElement('span',
+			['genre', 'animated', 'fadeInRight'], {
+				textContent: genre
+			}
+		);
+		content.append(genreSpan);
 	}
-	if(rating){
-			const ratingBlock = getElement('div', ['rating', 'animated', 'fadeInRight']); 
-			const ratingStars = getElement('div', ['rating-stars']); 
-			const ratingNumber = getElement('div', ['rating-number'],{
-				textContent: `${rating}/10`
+	//add rating
+	if (rating) {
+		const ratingBlock = getElement('div', ['rating', 'animated', 'fadeInRight']);
+		const ratingStars = getElement('div', ['rating-stars']);
+		const ratingNumber = getElement('div', ['rating-number'], {
+			textContent: `${rating}/10`
+		});
+		for (let i = 0; i < 10; i++) {
+			const star = getElement('img', ['star'], {
+				alt: i ? '' : `Рейтинг ${rating} из 10`,
+				src: i < rating ? 'assets/img/star.svg' : 'assets/img/star-o.svg'
 			});
-			ratingBlock.append(ratingStars, ratingNumber);
-			content.append(ratingBlock);
-	}
+			ratingStars.append(star);
+		}
+		ratingBlock.append(ratingStars, ratingNumber);
+		content.append(ratingBlock);
+	};
+	//add title
+	content.append(getElement('h1',
+		['main-title', 'animated', 'fadeInRight'], {
+			textContent: title
+		}
+	));
+	//add description
+	if (description) {
+		content.append(getElement('p',
+			['main-description', 'animated', 'fadeInRight'], {
+				textContent: description
+			},
+		));
+	};
+	//add buttons trailer
+	if (trailer) {
+		const youtubeLink = getElement('a',
+			['button', 'animated', 'fadeInRight', 'youtube-modal'], {
+				href: trailer,
+				textContent: 'Смотреть трейлер',
+			});
+
+		const youtubeImgLink = getElement('a', ['play', 'youtube-modal'], {
+			href: trailer,
+			ariaLabel: 'Смотреть трейлер',
+		});
+
+		const iconPlay = getElement('img', ['play-img'], {
+			src: 'assets/img/play.svg',
+			alt: 'Play',
+			ariaHidden: true,
+		});
+
+		content.append(youtubeLink);
+		youtubeImgLink.append(iconPlay);
+		wrapper.append(youtubeImgLink);
+
+	};
+
+
 	return main;
 };
 
+
+//start
 const movieConstructor = (selector, options) => {
 	const app = document.querySelector(selector);
 	app.classList.add('body-app');
 	app.style.backgroundImage = options.background ?
-	`url("${options.background}")` : '';
-	
+		`url("${options.background}")` : '';
+
 
 	document.title = options.title;
-	
-	if(options.header){
+
+	if (options.header) {
 		app.append(createHeader(options));
 	}
-	if(options.main){
+	if (options.main) {
 		app.append(createMain(options));
 	}
 
@@ -106,45 +165,43 @@ const movieConstructor = (selector, options) => {
 
 
 movieConstructor('.app', {
-	title: 'Tom Clancy`s The Divisions',
-	background: 'assets/img/background.png', 
+	title: 'The Division 2',
+	background: 'assets/img/background.png',
 	header: {
-			logo: 'assets/img/logo.png',
-			social: [
-				{
-					title: 'Twitter',
-					link: '#',
-					image: 'assets/img/social/twitter.svg',
-				},
-				{
-					title: 'Instagrsm',
-					link: '#',
-					image: 'assets/img/social/instagram.svg',
-				},
-				{
-					title: 'Facebook',
-					link: '#',
-					image: 'assets/img/social/facebook.svg',
-				}
-			],
-			menu: [
-				{
-					title: 'Описание',
-					link: '#',
-				},
-				{
-					title: 'Трейлер',
-					link: '#',
-				},
-				{
-					title: 'Отзывы',
-					link: '#',
-				},
-			]
+		logo: 'assets/img/logo.png',
+		social: [{
+				title: 'Twitter',
+				link: '#',
+				image: 'assets/img/social/twitter.svg',
+			},
+			{
+				title: 'Instagrsm',
+				link: '#',
+				image: 'assets/img/social/instagram.svg',
+			},
+			{
+				title: 'Facebook',
+				link: '#',
+				image: 'assets/img/social/facebook.svg',
+			}
+		],
+		menu: [{
+				title: 'Описание',
+				link: '#',
+			},
+			{
+				title: 'Трейлер',
+				link: '#',
+			},
+			{
+				title: 'Отзывы',
+				link: '#',
+			},
+		]
 	},
 	main: {
 		genre: '2018 Шутер Боевик',
-		rating: 9,
+		rating: 7,
 		description: 'В Tom Clancy`s The Division®2 Вашингтон предстанет таким, каким вы его еще нигде не видели: город воссоздан один в один, что позволило добиться непревзойденной правдоподобности окружающего мира. Вы сможете вблизи рассмотреть все достопримечательности, ориентиры, городские районы и вражеские логова.',
 		trailer: 'https://www.youtube.com/watch?v=7NTKO4Y-JmY',
 
